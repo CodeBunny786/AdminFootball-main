8d462820fc00513c6388110773c974799d45c576
Muhammad Ahmar <codebunny7@gmail.com>
2021-05-04 11:52:21 +0500

    build folder

---
+++ build/precache-manifest.83cfffad243a436a5567a86912aa8f67.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed12050d0860bb195c1d54062e94e839",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "50e81af811d3281a9afb",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "a0daa8f68b012e1cf47f",
+    "url": "/material-dashboard-react/static/css/main.71f94c48.chunk.css"
+  },
+  {
+    "revision": "50e81af811d3281a9afb",
+    "url": "/material-dashboard-react/static/js/2.4f123395.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/material-dashboard-react/static/js/2.4f123395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0daa8f68b012e1cf47f",
+    "url": "/material-dashboard-react/static/js/main.b4802031.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/material-dashboard-react/static/js/main.b4802031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a025ab54ec03fed337bb",
+    "url": "/material-dashboard-react/static/js/runtime-main.d0bf2bb1.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
